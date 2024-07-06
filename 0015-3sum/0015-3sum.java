06b953a79aece8e48f3143ebb00700a37292c5f0
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2024-07-07 01:29:31 +0300

    Time: 40 ms (38.37%), Space: 51.3 MB (62.28%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,62 +1,39 @@
 class Solution {
-      Set<List<Integer>>ans=new HashSet<>();
-      Set<Integer>cont=new HashSet<>();
-      Map<Integer,Integer>MapCount=new HashMap<>();
+    List<List<Integer>>ANS=new ArrayList<>();
+    Set<Integer>used=new HashSet<>();
+    int []pseudo;
     public List<List<Integer>> threeSum(int[] nums) {
-        for (int x:nums) {
-            cont.add(x);
-            if(!MapCount.containsKey(x))
-            {
-                MapCount.put(x,1);
-            }
-            else
-            {
-                MapCount.put(x,MapCount.get(x)+1);
-            }
-        }
-        for (int x:cont) {
-            for (int y:cont) {
-                int find=0-(x+y);
-                if(cont.contains(find))
-                {
-                    Map<Integer,Integer>MapCheck=new HashMap<>();
-                    if(!MapCheck.containsKey(x))
-                    {
-                        MapCheck.put(x,1);
-                    }
-                    else
-                    {
-                        MapCheck.put(x,MapCheck.get(x)+1);
-                    }
-                    if(!MapCheck.containsKey(y))
-                    {
-                        MapCheck.put(y,1);
-                    }
-                    else
-                    {
-                        MapCheck.put(y,MapCheck.get(y)+1);
-                    }
-                    if(!MapCheck.containsKey(find))
-                    {
-                        MapCheck.put(find,1);
-                    }
-                    else
-                    {
-                        MapCheck.put(find,MapCheck.get(find)+1);
-                    }
-                    if((MapCount.get(x)>=MapCheck.get(x))&&(MapCount.get(y)>=MapCount.get(y))&&(MapCount.get(find)>=MapCheck.get(find)))
-                    {
-                        List<Integer>add=new ArrayList<>(Arrays.asList(x,y,find));
-                        Collections.sort(add);
-                        ans.add(add);
-                    }
-                }
-            }
-        }
-        List<List<Integer>>ANS=new ArrayList<>();
-        for (List<Integer>x:ans) {
-            ANS.add(x);
+        Arrays.sort(nums);
+        pseudo=nums;
+        int siz=nums.length;
+        for (int i = 0; i <siz-2; i++) {
+            if(used.contains(nums[i]))
+                continue;
+            used.add(nums[i]);
+            int find=0-nums[i];
+            AddTwoSum(i+1,siz-1,find,nums[i]);
         }
         return ANS;
+    }
+    void AddTwoSum(int st,int end,int target,int found)
+    {
+        Set<Integer>USED2=new HashSet<>();
+        while(st<end)
+        {
+            if((pseudo[st]+pseudo[end])==target)
+            {
+                if(!(USED2.contains(pseudo[st]))) {
+                    ANS.add(new ArrayList<>(Arrays.asList(found, pseudo[st], pseudo[end])));
+                    USED2.add(pseudo[st]);
+                    USED2.add(pseudo[end]);
+                }
+                st++;end--;
+            }
+            else if (pseudo[st]+pseudo[end]>target) {
+                end--;
+            }
+            else
+                st++;
+        }
     }
 }
