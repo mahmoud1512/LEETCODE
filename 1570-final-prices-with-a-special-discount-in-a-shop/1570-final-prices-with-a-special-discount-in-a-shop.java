aaac6afb8947f16fa8d1e22af98348a53e8d3aee
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2024-07-11 21:51:14 +0300

    Time: 3 ms (48.9%), Space: 44 MB (18.97%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,25 +1,15 @@
- class node
-  {
-      int val,place;
-
-      public node(int val, int place) {
-          this.val = val;
-          this.place = place;
-      }
-  }
-
-class Solution {
+ class Solution {
     public int[] finalPrices(int[] prices) {
-           Stack<node>stack=new Stack<>();
+           Stack<Integer>stack=new Stack<>();
            int size= prices.length;
            int []ans=prices.clone();
         for (int i = 0; i < size; i++) {
-            while (!(stack.isEmpty())&&prices[i]<=stack.peek().val)
+            while (!(stack.isEmpty())&&prices[i]<=prices[stack.peek()])
             {
-                node n=stack.pop();
-                ans[n.place]=n.val-prices[i];
+                int n=stack.pop();
+                ans[n]=prices[n]-prices[i];
             }
-            stack.push(new node(prices[i],i ));
+            stack.push(i);
         }
         return ans;
     }
