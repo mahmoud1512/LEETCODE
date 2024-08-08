0b9bd63da99f4cfee7789213a5029aee9bff516d
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2024-08-09 01:24:26 +0300

    Time: 2 ms (86.94%), Space: 45.3 MB (44.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,20 +2,38 @@ class Solution {
     boolean[][]visited;
     char[][]dummyBoard;
     int n,m;
-   
+
     public void solve(char[][] board) {
         n=board.length;
         m=board[0].length;
         visited=new boolean[n][m];
         dummyBoard=board;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(board[i][j]=='O'&&!visited[i][j]&&Corner(i,j))
-                {
-                    dfs(i,j);
-                }
+
+        for (int j = 0; j < m; j++) {
+            if(board[0][j]=='O'&&!visited[0][j])
+            {
+                dfs(0,j);
             }
         }
+        for (int j = 0; j < m; j++) {
+            if(board[n-1][j]=='O'&&!visited[n-1][j])
+            {
+                dfs(n-1,j);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(board[i][0]=='O'&&!visited[i][0])
+            {
+                dfs(i,0);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(board[i][m-1]=='O'&&!visited[i][m-1])
+            {
+                dfs(i,m-1);
+            }
+        }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if(!visited[i][j]&&board[i][j]=='O')
@@ -28,7 +46,7 @@ class Solution {
         if(visited[i][j])
             return;
         visited[i][j]=true;
-        
+
         if(valid(i+1,j))
         {
             dfs(i+1,j);
