846052dc1da7100341fd2e06cb72aabaae8c908a
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2024-09-11 17:13:46 +0300

    Time: 0 ms (100%), Space: 43.5 MB (52.43%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,34 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    boolean ans=true;
     public boolean isValidBST(TreeNode root) {
-
-        traverse(root);
-        return ans;
+          return check(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    private long[] traverse(TreeNode root)
+    boolean check(TreeNode x,long mn,long mx)
     {
-        if (root==null)
-            return new long[]{Long.MAX_VALUE,Long.MIN_VALUE};
-        long[] p1=traverse(root.right);
-        long[] p2=traverse(root.left);
-        if(root.val<=p2[1]||root.val>=p1[0])
-            ans=false;
-        return new long[]{Math.min(root.val,Math.min(p1[0],p2[0])),Math.max(root.val,Math.max(p1[1],p2[1]))};
+        if(x==null)
+            return true;
+        boolean p1=x.val>mn;
+        boolean p2=x.val<mx;
+        return p1&&p2&&check(x.left,mn,x.val)&&check(x.right,x.val,mx);
     }
-
 }
