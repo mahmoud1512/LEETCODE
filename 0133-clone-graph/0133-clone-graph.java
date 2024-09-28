cbb70876bb6c519e3959421181179c263730b80b
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2024-09-28 03:10:49 +0300

    Time: 23 ms (100%), Space: 42.5 MB (75.23%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,61 +1,29 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
 class Solution {
-    Node ans=new Node(1);
-    Map<Integer,Node>NodeGetter=new HashMap<>();
-    Map<Integer,Boolean>finishedFlag=new HashMap<>();
-    Queue<Node>nodeQueue=new ArrayDeque<>();
-    Set<Integer>added=new HashSet<>();
+    Map<Node,Node>nodeMap;
+
     public Node cloneGraph(Node node) {
         if(node==null)return null;
-        NodeGetter.put(1,ans);
-        finishedFlag.put(1,false);
-        nodeQueue.add(node);
-        int siz=1,depth;
-        while (!nodeQueue.isEmpty())
+        nodeMap=new HashMap<>();
+        dfs(node);
+       return nodeMap.get(node);
+    }
+
+    private void dfs(Node node) {
+        if(nodeMap.containsKey(node))
+            return;
+       
+        nodeMap.put(node,new Node(node.val));
+        Node added=nodeMap.get(node);
+        
+        for (Node n: node.neighbors)
         {
-            depth=0;
-            for (int i = 0; i < siz; i++) {
-                Node v=nodeQueue.poll();
-                Node n=NodeGetter.get(v.val);
-               for(Node x:v.neighbors)
-               {
-                   if(!NodeGetter.containsKey(x.val))
-                   {
-                       NodeGetter.put(x.val,new Node(x.val));
-                       finishedFlag.put(x.val,false);
-                   }
-                   if(!finishedFlag.get(x.val)&&!added.contains(x.val))
-                   {
-                       nodeQueue.add(x);
-                       added.add(x.val);
-                       depth++;
-                   }
-                   n.neighbors.add(NodeGetter.get(x.val));
-               }
-               finishedFlag.put(n.val,true);
-            }
-            siz=depth;
-
+            dfs(n);
+            
+            if(added.neighbors==null)
+                added.neighbors=new ArrayList<>();
+            added.neighbors.add(nodeMap.get(n));
+            
         }
-        return ans;
-
+            
     }
 }
