ccd4ebb9b969fa05ffc31604d74f2d7870e45ed4
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2024-10-18 10:21:54 +0300

    Time: 2 ms (77.98%), Space: 51 MB (69.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,41 +1,34 @@
-//this problem can be solved by sort and one pass O(nlg)
-//or by one pass using a helper variable candidate O(n)
 class Solution {
 
     public int majorityElement(int[] nums) {
-          int size=nums.length;
-          int Candidate=nums[0];
-          int counter=1;
-          int i=1;
-          boolean p=true;
-         while (i<size)
-         {
-             if (nums[i]==Candidate) {
-                 if(p)
-                     counter++;
-                 else 
-                     counter--;
-             }
-             else{
-                 if(p){
-                     counter--;
-                     if(counter<0) {
-                         Candidate = nums[i];
-                         p=false;
-                     }
-                 }
-                 else
-                 {
-                     counter++;
-                     if(counter>0) {
-                         Candidate = nums[i];
-                         p=true;
-                     }
-                 }
-                
-             }
-             i++;
-         }
-         return Candidate;
+        return majority(nums,0,nums.length-1)[1];
+    }
+
+    private int[] majority(int[] nums, int start, int end) {
+
+        //base case
+        if(start==end)
+        {
+            return new int[]{1,nums[start]};
+        }
+         int mid=(start+end)/2;
+        int[]x1=majority(nums,start,mid);
+        int[]x2=majority(nums,mid +1,end);
+
+        if(x1[1]!=x2[1]){
+            if(x1[0]>x2[0])
+                return new int[]{x1[0]-x2[0],x1[1]};
+            else if (x1[0]<x2[0]) {
+                return new int[]{x2[0]-x1[0],x2[1]};
+            }
+            else
+            {
+                //They will delete each other
+                return new int[]{0,x1[1]};
+            }
+        }
+        else 
+            return new int[]{x1[0]+x2[0],x2[1]};
+       
     }
 }
