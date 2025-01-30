2b52948d476fc6c138015d005986e8cc853a7939
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-01-30 14:06:48 +0200

    Time: 12 ms (27.03%), Space: 44.3 MB (87.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,68 +1,24 @@
-class Solution {
-    char[][]my_board;
+public class Solution {
     public boolean isValidSudoku(char[][] board) {
-            my_board=board;
-            return rows()&&cols()&&boxes();
-    }
-    boolean rows()
-    {
-        for (int i = 0; i < 9; i++) {
-            Set<Character>set=new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (my_board[i][j]=='.')
-                    continue;
-                if(set.contains(my_board[i][j]))
-                    return false;
-                set.add(my_board[i][j]);
-            }
-        }
-        return true;
-    }
-    boolean cols()
-    {
-        for (int i = 0; i < 9; i++) {
-            Set<Character>set=new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (my_board[j][i]=='.')
-                    continue;
-                if(set.contains(my_board[j][i]))
-                    return false;
-                set.add(my_board[j][i]);
-            }
-        }
-        return true;
-    }
-    boolean boxes()
-    {
-          boolean ans1=true,ans2=true,ans3=true;
-        for (int i = 0; i <=6 ; i+=3) {
-            ans1=ans1&check_box(0,3,i,i+3);
-            if(ans1==false)
-                return false;
-        }
-        for (int i = 0; i <=6 ; i+=3) {
-            ans2=ans2&check_box(3,6,i,i+3);
-            if(ans2==false)
-                return false;
-        }
-        for (int i = 0; i <=6 ; i+=3) {
-             ans3 = ans3 & check_box(6, 9, i, i + 3);
-            if(ans3==false)
-                return false;
-        }
-        return true;
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
 
-    }
-    boolean check_box(int r1,int r2,int c1,int c2)
-    {
-        Set<Character>set=new HashSet<>();
-        for (int i = r1; i < r2; i++) {
-            for (int j = c1; j < c2; j++) {
-                if (my_board[i][j]=='.')
-                    continue;
-                if(set.contains(my_board[i][j]))
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+
+                String squareKey = (r / 3) + "," + (c / 3);
+
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                    cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                    squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])) {
                     return false;
-                set.add(my_board[i][j]);
+                }
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
             }
         }
         return true;
