9024eb5a79ee06ddc00c3e4634bce723bed83807
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-02-02 10:26:23 +0200

    Time: 0 ms (100%), Space: 41.7 MB (17.38%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -2,11 +2,11 @@ class MyStack {
 
 
     Queue<Integer>q1;
-    Queue<Integer>q2;
+    
     public MyStack() {
 
         q1=new ArrayDeque<>();
-        q2=new ArrayDeque<>();
+        
     }
 
     public void push(int x) {
@@ -16,33 +16,25 @@ class MyStack {
 
     public int pop() {
 
-        if(q2.isEmpty())
-        {
-            while (q1.size()!=1)
-                q2.add(q1.poll());
+       int siz=q1.size();
+        for (int i = 0; i < siz-1; i++) {
+            q1.add(q1.poll());
         }
-        Queue<Integer>temp=q1;
-        q1=q2;
-        q2=temp;
-        return q2.poll();
+        return q1.poll();
     }
 
     public int top() {
-        if(q2.isEmpty())
-        {
-            while (q1.size()!=1)
-                q2.add(q1.poll());
+        int siz=q1.size();
+        for (int i = 0; i < siz-1; i++) {
+            q1.add(q1.poll());
         }
-        Queue<Integer>temp=q1;
-        q1=q2;
-        q2=temp;
-        int x=q2.poll();
-        q1.add(x);
+        int x=q1.peek();
+         q1.add(q1.poll());
         return x;
     }
 
     public boolean empty() {
 
-        return q1.isEmpty()&&q2.isEmpty();
+        return q1.isEmpty();
     }
 }
