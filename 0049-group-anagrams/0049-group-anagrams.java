06ea9dbef52a1290296a6b0bb6b95e3aca24caee
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-02-15 21:16:14 +0200

    Time: 11 ms (31.47%), Space: 47.5 MB (94.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,38 @@
 class Solution {
-    Map<List<Integer>,List<String>>map=new HashMap<>();
     public List<List<String>> groupAnagrams(String[] strs) {
-        int siz=strs.length;
-        for (int i = 0; i <siz ; i++) {
-            List<Integer>list=decompose(strs[i]);
-              if(map.containsKey(list))
-              {
-                  map.get(list).add(strs[i]);
-              }
-              else
-              {
-                  map.put(list,new ArrayList<>(Arrays.asList(strs[i])));
-              }
+         Map<List<Integer>,List<String>>AnagramGrouper=new HashMap<>();
+        for (String x:strs) {
+            List<Integer> stringDecomposition =getDecomposition(x);
+            if(AnagramGrouper.containsKey(stringDecomposition))
+            {
+                AnagramGrouper.get(stringDecomposition).add(x);
+            }
+            else
+            {
+                List<String>anagram=new ArrayList<>();
+                anagram.add(x);
+                AnagramGrouper.put(stringDecomposition,anagram);
+            }
         }
         List<List<String>>ans=new ArrayList<>();
-        for (List<Integer>entry:map.keySet()) {
-            ans.add(map.get(entry));
+        for (List<Integer> x:AnagramGrouper.keySet()) {
+            ans.add(AnagramGrouper.get(x));
         }
         return ans;
     }
-    List<Integer> decompose(String x)
-    {
-       int []arr=new int[26];
-       List<Integer>ans=new ArrayList<>();
-        int l=x.length();
-        for (int i = 0; i < l; i++) {
-            arr[x.charAt(i)-'a']++;
-        }
+
+    private List<Integer> getDecomposition(String x) {
+        List<Integer>decomposition=new ArrayList<>(26);
+        int siz=x.length();
         for (int i = 0; i < 26; i++) {
-            ans.add(arr[i]);
+            decomposition.add(0);
         }
-     return ans;
-        
+
+        int index;
+        for (int i = 0; i < siz; i++) {
+            index=x.charAt(i)-'a';
+            decomposition.set(index, decomposition.get(index) + 1);
+        }
+        return decomposition;
     }
 }
