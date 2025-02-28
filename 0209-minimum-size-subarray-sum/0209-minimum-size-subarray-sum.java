d5507b546d2b80ff057b37944bbff24eb3032770
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-02-28 06:56:41 +0200

    Time: 1 ms (99.75%), Space: 58.3 MB (29.47%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,41 +1,26 @@
 class Solution {
-      long[]preSum;
-     int siz;
     public int minSubArrayLen(int target, int[] nums) {
-           siz= nums.length;
-          preSum=new long[siz+1];preSum[0]=0;
-        for (int i = 1; i < siz+1; i++) {
-            preSum[i]=preSum[i-1]+nums[i-1];
+
+        int len=nums.length;
+        int left=0;   // left is initialized
+
+        int sum=0;   //window state(empty)
+        int globalMin=Integer.MAX_VALUE;    // current global min
+
+        // condition is sum < target , when I surpass it  will only be one element, so it will be detected inside while loop
+        for (int i = 0; i < len; i++) {
+
+            sum+=nums[i];
+
+            while (sum>=target&&left<=i)
+            {
+                globalMin=Math.min(globalMin,i-left+1);
+                sum-=nums[left];
+                left++;
+            }
         }
 
-        //problem is monotonically increasing in sense of size
-        int l=1,r=siz;
-        int ans=siz+1;
-        while (l<=r)
-        {
-            int m=(l+r)/2;
-            if(can(m,target))
-            {
-                ans=Math.min(ans,m);
-                r=m-1;
-            }
-            else
-            {
-                l=m+1;
-            }
-        }
-        if(ans==siz+1)
-           return 0;
-        return ans;
-    }
-    boolean can(int x,int target)
-    {
-       long val;           
-        for (int i = 0; i <= siz-x; i++) {
-            val=preSum[x+i]-preSum[i];
-            if ((int)val>=target)
-                return true;
-        }
-        return false;
+        return globalMin==Integer.MAX_VALUE?0:globalMin;
+
     }
 }
