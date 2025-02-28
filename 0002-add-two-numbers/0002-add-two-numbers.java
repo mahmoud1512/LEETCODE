209f0714cc39347fd7c1a58fb2a7fea70cd4438d
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-02-28 11:37:17 +0200

    Time: 1 ms (100%), Space: 44.5 MB (52.03%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,45 +1,50 @@
 class Solution {
       public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-             ListNode head=new ListNode(0); ListNode current=head,before=null;
-             while (l1!=null&&l2!=null)
-             {
-                  ListNode next;
-                  int v= current.val+l1.val+l2.val;
-                  current.val=v%10;
-                  before=current;
-                  next=new ListNode(v/10);
-                  current.next=next;
-                  current=next;
-                  l1=l1.next;
-                  l2=l2.next;
-             }
-             while (l1!=null)
-             {
-                 ListNode next;
-                 int v= current.val+l1.val;
-                 current.val=v%10;
-                 before=current;
-                 next=new ListNode(v/10);
-                 current.next=next;
-                 current=next;
-                 l1=l1.next;
-             }
-             while (l2!=null)
-             {
-                 ListNode next;
-                 int v= current.val+l2.val;
-                 current.val=v%10;
-                 before=current;
-                 next=new ListNode(v/10);
-                 current.next=next;
-                 current=next;
-                 l2=l2.next;
-             }
+          int currentValue=0;
+          ListNode head=l1;
+          ListNode concat1 =null;
+          ListNode concat2=null;
+          while (l1!=null&&l2!=null)
+          {
+              int v= currentValue+l1.val+l2.val;
+              l1.val=v%10;
+              currentValue=v/10;
+              if(l1.next==null)
+                  concat1 =l1;
+              l1=l1.next;
+              l2=l2.next;
+          }
+         
+          while (l1!=null)
+          {
+             
+              int v= currentValue+l1.val;
+              l1.val=v%10;
+              currentValue=v/10;
+              if(l1.next==null)
+                  concat1 =l1;
+              l1=l1.next;
+          }
+          while (l2!=null)
+          {
+              if(concat2==null)
+                  concat2=l2;
+              int v= currentValue+l2.val;
+              currentValue=v/10;
+              l2.val=v%10;
+              if(l2.next==null&&currentValue!=0) {
+                  l2.next = new ListNode(currentValue);
+                  break;
+              }
+              l2=l2.next;
+          }
 
-             if(current.val==0)
-              before.next=null;
+          if(concat2==null&&currentValue!=0)
+              concat2=new ListNode(currentValue);
+
+          concat1.next=concat2;
 
 
-           return head;
+          return head;
       }
   }
