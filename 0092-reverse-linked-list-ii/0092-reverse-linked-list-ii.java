3da07479690edffe71651a1e5dbb699a84b5c51d
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-01 22:57:48 +0200

    Time: 0 ms (100%), Space: 41.6 MB (19.5%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,53 +10,29 @@
  */
 class Solution {
       public ListNode reverseBetween(ListNode head, int left, int right) {
-
-          ListNode beforeLeft=null; // dummy now
-          ListNode afterRight=null;
-          ListNode x1=head;
-          ListNode x2=head;
-          boolean thereIsLeft=false;
-          for (int i = 1; i < left; i++) {
+           ListNode dummy=new ListNode();
+           dummy.next=head;
+           ListNode l=dummy;
+           ListNode bl=null;
+          for (int i = 0; i < left; i++) {
               if(i==left-1)
-              {
-                  beforeLeft=x1;
-                  thereIsLeft=true;
-              }
-              x1=x1.next;
-              x2=x2.next;
+                  bl=l;
+              l=l.next;
           }
-          if(thereIsLeft)
-            beforeLeft.next=null;
-          for (int i = left; i < right; i++) {
-              x2=x2.next;
-          }
-          
-        afterRight=x2.next;
-        x2.next=null;
-          
-          ListNode x=x1.next;
-          ListNode v=x1;
-         
+          ListNode x=l.next;
+          int reversals=right-left;
           ListNode temp;
-          while (x!=null)
-          {
+          ListNode ll=l;
+          for (int i = 0; i < reversals; i++) {
               temp=x;
               x=x.next;
-              temp.next=x1;
-              x1=temp;
+              temp.next=l;
+              l=temp;
           }
-          if (thereIsLeft)
-          {
-              beforeLeft.next=x1;
-              v.next=afterRight;
-
-              return head;
-          }
-          else
-          {
-              v.next=afterRight;
-              return x1;
-          }
-
+        //   if(left!=1)
+            bl.next=l;
+          ll.next=x;
+          return dummy.next;
+          
       }
   }
