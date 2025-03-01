7e8102c1c3a28bbccb20ec858e982ef2cac98ba2
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-01 23:05:21 +0200

    Time: 0 ms (100%), Space: 45 MB (11.44%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,31 +1,37 @@
-class Solution {
-    Map<Node,Node> NodeMap;
-    public Node copyRandomList(Node head) {
-        NodeMap=new HashMap<>();
-        Node ans=head;
-        Node unCopiedCurrent=head;
-        Node CopiedCurrent=null,CopiedPrev=null;
-        while (unCopiedCurrent!=null)
-        {
-            CopiedCurrent=new Node(unCopiedCurrent.val);
-            NodeMap.put(unCopiedCurrent,CopiedCurrent);
-            if(CopiedPrev==null)
-            {
-                CopiedPrev=CopiedCurrent;
-            }
-            else
-            {
-                CopiedPrev.next=CopiedCurrent;
-                CopiedPrev=CopiedCurrent;
-            }
-            unCopiedCurrent=unCopiedCurrent.next;
-        }
-        while (head!=null)
-        {
-            Node x=NodeMap.get(head);
-            x.random=NodeMap.get(head.random);
-            head=head.next;
-        }
-        return NodeMap.get(ans);
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
 }
+*/
+class Solution {
+      public Node copyRandomList(Node head) {
+          Map<Node,Node> nodeMap=new HashMap<>();
+          nodeMap.put(null,null);
+          Node curr=head;
+          while (curr!=null)
+          {
+              nodeMap.put(curr,new Node(curr.val));
+              curr=curr.next;
+          }
+          curr=head;
+          Node pointer=null;
+          while (curr!=null)
+          {
+              pointer=nodeMap.get(curr);
+              pointer.next=nodeMap.get(curr.next);
+              pointer.random=nodeMap.get(curr.random);
+              curr=curr.next;
+          }
+          return nodeMap.get(head);
+
+      }
+  }
