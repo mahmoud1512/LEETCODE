1dfec3bd0596a98bc30ecc3b7c6d01c2c5ea944c
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-04 21:11:31 +0200

    Time: 0 ms (100%), Space: 42.9 MB (80.94%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,23 +1,24 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-           int l=0,r=nums.length-1; int ans=-1;
-           while (l<=r)    //we want to find nearest element to target from left
-           {
-               int m=(l+r)/2;
-               if(nums[m]<=target)
-               {
-                   ans=m;
-                   l=m+1;
-               }
-               else
-               {
-                   r=m-1;
-               }
-           }
-           if(ans!=-1&&nums[ans]==target)
-               return ans;   
-           else  
-               return ans+1;
-         
+          
+        int l=0,r=nums.length-1;
+        int m;
+        while (l<=r)
+        {
+            m=(l+r)/2;
+            if(nums[m]==target)
+            {
+                return m;
+            }
+            else if(nums[m]<target)
+            {
+                l=m+1;
+            }
+            else
+            {
+                r=m-1;
+            }
+        }
+        return r+1;
     }
 }
