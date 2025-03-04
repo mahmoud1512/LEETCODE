eb608d67b1815dd759f632eaeec4ad18dce9153f
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-04 23:45:49 +0200

    Time: 4 ms (21.99%), Space: 48.3 MB (7.67%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,80 +1,56 @@
 class Solution {
     boolean[][]visited;
-    char[][]dummyBoard;
-    int n,m;
-
+    boolean[][]visited2;
+    boolean state;
     public void solve(char[][] board) {
-        n=board.length;
-        m=board[0].length;
-        visited=new boolean[n][m];
-        dummyBoard=board;
 
-        for (int j = 0; j < m; j++) {
-            if(board[0][j]=='O'&&!visited[0][j])
-            {
-                dfs(0,j);
-            }
-        }
-        for (int j = 0; j < m; j++) {
-            if(board[n-1][j]=='O'&&!visited[n-1][j])
-            {
-                dfs(n-1,j);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if(board[i][0]=='O'&&!visited[i][0])
-            {
-                dfs(i,0);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if(board[i][m-1]=='O'&&!visited[i][m-1])
-            {
-                dfs(i,m-1);
+        visited=new boolean[board.length][board[0].length];
+        visited2=new boolean[board.length][board[0].length];
+        
+        for (int i = 0; i <board.length ; i++) {
+            for (int j = 0; j <board[0].length ; j++) {
+                state=false;
+                if(board[i][j]=='O'&&!visited[i][j])
+                {
+                    dfs(i,j,board);
+                    if(!state)
+                    {
+                        makeX(i,j,board);
+                    }
+                }
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(!visited[i][j]&&board[i][j]=='O')
-                    board[i][j]='X';
-            }
-        }
     }
-    void dfs(int i,int j)
-    {
-        if(visited[i][j])
+
+    private void makeX(int i, int j, char[][] board) {
+        if(i<0||j<0||i>=board.length||j>=board[0].length)
+        {
             return;
+        } else if (board[i][j]=='X'||visited2[i][j]) {
+            return;
+        }
+        visited2[i][j]=true;
+        board[i][j]='X';
+        makeX(i-1,j,board);
+        makeX(i+1,j,board);
+        makeX(i,j+1,board);
+        makeX(i,j-1,board);
+    }
+
+    private void dfs(int i, int j, char[][] board) {
+        if(i<0||j<0||i>=board.length||j>=board[0].length)
+        {
+            state=true;
+            return;
+        } else if (board[i][j]=='X'||visited[i][j]) {
+            return;
+        }
         visited[i][j]=true;
-
-        if(valid(i+1,j))
-        {
-            dfs(i+1,j);
-        }
-        if(valid(i-1,j))
-        {
-            dfs(i-1,j);
-        }
-        if(valid(i,j+1))
-        {
-            dfs(i,j+1);
-        }
-        if(valid(i,j-1))
-        {
-            dfs(i,j-1);
-        }
+        dfs(i-1,j,board);
+        dfs(i+1,j,board);
+        dfs(i,j+1,board);
+        dfs(i,j-1,board);
+        
     }
-
-    private boolean Corner(int i, int j) {
-        if(i==0||i==n-1||j==0||j==m-1)
-            return true;
-        return false;
-    }
-
-    private boolean valid(int i, int j) {
-        if(i<n&&i>=0&&j<m&&j>=0&&dummyBoard[i][j]=='O')
-            return true;
-        return false;
-    }
-
 }
