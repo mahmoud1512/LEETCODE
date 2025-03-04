52bc5097a46e7831d311da639f0b61aca37d41c8
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-04 22:23:13 +0200

    Time: 2 ms (99.82%), Space: 49.2 MB (85.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,31 @@
 class Solution {
-    char [][]grid2;
-    int [][]visited=new int[302][302];
+    
     public int numIslands(char[][] grid) {
 
-        grid2=grid.clone();
-
         int count=0;
-        int rows=grid.length;
-        int cols=grid[0].length;
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;j++)
-            {
-                if(grid2[i][j]=='1')
+        int n=grid.length,m=grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(grid[i][j]=='1')
                 {
-                    dfs(i,j);
                     count++;
+                    dfs(i,j,grid);
                 }
+                    
             }
         }
-      return count;
-    }
-    public void dfs(int i,int j)
-    {
-        if (i>=grid2.length||i<0||j>=grid2[0].length||j<0||visited[i][j]==1||grid2[i][j]=='0')
-            return;
-        grid2[i][j]='0';
-        visited[i][j]=1;
-        
-        dfs(i+1,j);
-        dfs(i,j+1);
-        dfs(i-1,j);
-        dfs(i,j-1);
+        return count;
     }
 
+    private void dfs(int i, int j, char[][] grid) {
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0')
+            return;
+        
+        grid[i][j]='0';
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
+        
+    }
 }
