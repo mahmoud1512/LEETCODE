2bcfed4484b466792887cadc7b1095b7e22ce6b7
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-09 12:18:06 +0200

    Time: 0 ms (100%), Space: 43.1 MB (72.78%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -1,24 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int[]values;
-    int len;
+    int []arr;
     public TreeNode sortedArrayToBST(int[] nums) {
-         len=nums.length;
-         values=nums;
-
-        return solve(0,len-1);
-
-
+        
+        arr=nums;
+          return  solve(0,nums.length-1);
     }
-
-   private TreeNode solve(int start, int end) {
-            int mid=(start+end)/2;
-            TreeNode x=new TreeNode(values[mid]);
-            if(x!=null) {
-                if(mid>start)
-                    x.left = solve(start, mid - 1);
-                if(mid<end)
-                    x.right = solve(mid + 1, end);
-            }
-            return x;
-        }
+    TreeNode solve(int l, int r)
+    {
+        if(l>r)
+            return null;
+        if(l==r)
+            return new TreeNode(arr[l]);
+        int m=(l+r)/2;
+        TreeNode root=new TreeNode(arr[m]);
+        
+        root.left=solve(l,m-1);
+        root.right=solve(m+1,r);
+        return root;
+        
+    }
 }
