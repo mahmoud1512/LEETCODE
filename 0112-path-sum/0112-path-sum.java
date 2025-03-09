5c0c142f0a60f24e21f332c2642ffd614f68c95a
mahmoud abdelghany <120992403+mahmoud1512@users.noreply.github.com>
2025-03-09 12:11:27 +0200

    Time: 0 ms (100%), Space: 42.5 MB (99.84%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,39 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    int val=0;
+    int target;
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null)
-         return false;
-         return solve(root,targetSum,root.val);
+           return false;
+        target=targetSum;
+            return solve(root);
+        
     }
-
-    private boolean solve(TreeNode root, int targetSum,long currSum) {
-        if(root.left==null&&root.right==null)
+    boolean solve(TreeNode x)
+    {
+            
+        if(x.left==null&&x.right==null)
         {
-            return currSum==targetSum;
+            return val+x.val==target;
         }
-        boolean p1=false,p2=false;
-        if(root.left!=null)
+        
+        val+= x.val;
+        boolean p1=false;
+        boolean p2=false;
+        if(x.left!=null)
         {
-            p1=solve(root.left,targetSum,currSum+root.left.val);
+            p1=solve(x.left);
         }
-        if (root.right!=null)
+        if (x.right!=null)
         {
-            p2=solve(root.right,targetSum,currSum+root.right.val);
+            p2=solve(x.right);
         }
+        val-=x.val;
+        
         return p1||p2;
+        
+
     }
 }
